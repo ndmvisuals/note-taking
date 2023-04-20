88b1907dd9104b81f2499b4a641880cd4ffdc8d7
ndmvisuals <70617650+ndmvisuals@users.noreply.github.com>
2023-04-20 13:32:11 -0400

    Added functionality
    can use the create command to create a heiarchy of notes

--- note_taking/main.py
+++ note_taking/main.py
@@ -1,4 +1,6 @@
+import os
 import typer
+from pathlib import Path
 
 
 app = typer.Typer()
@@ -6,22 +8,48 @@ app = typer.Typer()
 
 @app.callback()
 def callback():
+
     """
-    Awesome Portal Gun
+    Note Taking CLI based on PARA
     """
+
+BASE_DIR = Path("/Users/nick/Documents/projects/GitHub/ndm_os/notes/")
 
 
 @app.command()
-def shoot():
+def create(keyword: str =  typer.Argument(...),
+                project: bool = typer.Option(False, "-p", "--project"),
+                area: bool = typer.Option(False, "-a", "--area"),
+                resource: bool = typer.Option(False, "-r", "--resource")):
     """
-    Shoot the portal gun
+    Create files with the given keyword and flag.
     """
-    typer.echo("Shooting portal gun")
+    if sum([project, area, resource]) > 1:
+        typer.echo("Please provide only one flag (-p, -a, -r)")
+        return
+    elif sum([project, area, resource]) == 0:
+        typer.echo("Please provide one flag (-p, -a, -r)")
+        return
+    else:
+        if project:
+            prefix = "project"
+            file_types = [keyword,'todo', 'note', 'meeting', 'log', "links"]
+            
+        elif area:
+            prefix = "area"
+            file_types = [keyword,'todo', 'note', 'meeting', 'log', "links"]
+        elif resource:
+            prefix = "resource"
+            file_types = [keyword,"links"]
 
+        for file_type in file_types:
+            if file_type == keyword:
+                file_name = f"{prefix}.{keyword}.md"
+            else:
+                file_name = f"{prefix}.{keyword}.{file_type}"
+            file_path = BASE_DIR / f"{file_name}.md"
+            file_path.touch(exist_ok=False)
+            typer.echo(f"Created {file_name}")
+        
 
-@app.command()
-def load():
-    """
-    Load the portal gun
-    """
-    typer.echo("Loading portal gun")
+    
